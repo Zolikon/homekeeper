26f567f76f2099fc779ef5f70883df13ff234798
Zoltan Pozsonyi <pozsonyizoli@gmail.com>
2026-04-03 21:54:43 +0200

    feat: add BottomNav component

---
+++ app/__components/BottomNav.tsx
@@ -0,0 +1,9 @@
+function BottomNav({ children }: { children: React.ReactNode }) {
+  return (
+    <nav className="w-full h-14 bg-theme_primary flex items-center justify-around shrink-0">
+      {children}
+    </nav>
+  );
+}
+
+export default BottomNav;
